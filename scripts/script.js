fa42fb166fa0b506f688282e080e81235b54987f
Jaaon <jingxuanshan@hotmail.com>
2024-09-22 10:32:15 -0700

    animation

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+var banner_image_number = 1
+const MAX_BANNER = 4
+
+window.onload = () => {
+  setInterval(() => {
+    $('#banner').css('background-image','url("'+'images/banner/mc_'+banner_image_number+'.jpg'+'")')
+    banner_image_number+=1
+    if(banner_image_number==MAX_BANNER) banner_image_number=0
+  },5000)
+}
