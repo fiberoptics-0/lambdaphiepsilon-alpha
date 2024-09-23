69adfe8fbc3e3d8ccb91b86b95421c385e4f01c7
Jaaon <jingxuanshan@hotmail.com>
2024-09-22 21:06:38 -0700

    selections

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,14 @@
 var banner_image_number = 1
 const MAX_BANNER = 4
 
-window.addEventListener('load',() => {
-  setInterval(() => {
+window.addEventListener('load',function() {
+  setInterval(function() {
     $('#banner').css('background-image','url("'+'images/banner/mc_'+banner_image_number+'.jpg'+'")')
     banner_image_number+=1
     if(banner_image_number==MAX_BANNER) banner_image_number=0
   },5000)
+  $('.selectable').mouseenter(function() {
+    $('.selectable').css('backdrop-filter','blur(5px)')
+    $(this).css('backdrop-filter','blur(0px)')
+  })
 })
